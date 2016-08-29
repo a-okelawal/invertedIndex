349793982ef5f51d37592b703e9f401c8e1597d3
abisolawal <abiso.lawal@gmail.com>
2016-08-29 12:40:31 +0100

    Format inverted-index.js with proper comments

--- inverted-index.js
+++ inverted-index.js
@@ -1,47 +1,64 @@
 var fs = require('fs');
 
-/*
-  Function to create a word index array out of json files
-*/
+//Function to create a word index array out of json files
 exports.WordIndexFactory = function(){
-  /*
-    Global variables for this function
-  */
+
+  //Variable that contains the  current index's json form
   this.jsonForm =  {};
+
+  //Variable that contains the current index's word index array
   this.wordIndex = [];
+
+  ////Variable Array that contains the word index array of all files
   this.collection = {};
+
+  //Variable that contains the json forms of all files
   this.jsonCollection = {};
+
+  //Temporary variable to use to store the file names
   this.name = "";
+
+  //Temporary array to store search results
   let tempSearch = [];
 
-  /*
-    Function to create the wordIndex from the file
-  */
+  //Function to create the wordIndex from the file
   this.createIndex = function(filePath){
     let answer = true;
     this.jsonForm = {};
     this.wordIndex = [];
+
+    //Check if file exists
     if(fs.existsSync(filePath))
     {
+      //Stores the name of the file and converst to json form
       this.name = filePath.split("/").pop();
       let content = fs.readFileSync(filePath);
       this.jsonForm = JSON.parse(content);
+
+      //stores json object with the name
       this.jsonCollection[this.name] = this.jsonForm;
+
+      //check if the json object is empty
       if(this.jsonForm == undefined)
         answer = false;
       else
       {
+        //Go through each json object
         for(let i = 0; i < this.jsonForm.length; i++)
         {
           if(this.jsonForm[i].title != undefined || this.jsonForm[i].text != undefined)
           {
+            //combine the strings in the object
             let comboText = this.jsonForm[i].title.toString().toLowerCase() + " " +
             this.jsonForm[i].text.toString().toLowerCase();
+
+            //Split all the words and put them into an array
             let tokens = comboText.split(" ");
             for(let j = 0; j < tokens.length; j++)
             {
               let temp = tokens[j].toString().replace(/[^a-zA-Z 0-9]+/g,'');
               let check = true;
+              //Check if it's the first iteration and put the first word
               if(i === 0 && j === 0)
               {
                 let obj = {};
@@ -50,6 +67,10 @@ exports.WordIndexFactory = function(){
               }
               else
               {
+                /*
+                *Check if element exists and if it does, just add the index to the file else add
+                *the word and the index.
+                */
                 for(let key of this.wordIndex)
                 {
                   for(let element in key)
@@ -69,31 +90,36 @@ exports.WordIndexFactory = function(){
                   let obj = {};
                   obj[temp] = [i];
                   this.wordIndex.push(obj);
-                } //end of if(check === true)
-              } //end of else
-            } //end of j for
-          } //end of title and body check
-        } //end of i for
+                }
+              }
+            }
+          }
+        }
         console.log("Word Index for " + this.name +" Created.\n")
-      } //end of else
-    } //end of file check
+      }
+    }
     else
     {
       console.log("File does not exist.\n");
       answer = false;
     }
+
+    //Add word index array to the collection using the name of the file
     this.collection[this.name] = this.wordIndex;
     return answer;
   };
 
-  /*
-    Function to return the jsonForm of the files
-  */
-  this.getJsonForm = function(file){
+  //Function to return the jsonForm of the files
+  this.getJsonForm = function(file)
+  {
+    //Check if argument is empty, if it so return last json object.
     if(file == undefined)
     return this.jsonCollection[this.jsonCollection.length - 1];
+
+    //Get index from argument and get json object
     this.jsonForm = this.jsonCollection[file];
-    //console.log("Checking " + this.jsonCollection[file]);
+
+    //Check if the json object with the name exists
     if(this.jsonCollection[file] == undefined)
     {
       console.log("Index Array for file \""+ file + "\" does not exist")
@@ -110,14 +136,17 @@ exports.WordIndexFactory = function(){
     }
   };
 
-  /*
-    Function to return word index of the files
-  */
+  //Function to return word index of the files
   this.getIndex = function(file)
   {
+    //Check if argument is empty, if it so return last word index array.
     if(file == undefined)
     return this.collection[this.collection.length - 1];
+
+    //Get index from argument and get the word index array
     this.wordIndex = this.collection[file];
+
+    //Check if the word index with the name exists
     if(this.collection[file] == undefined)
     {
       console.log("Index Array for file \""+ file + "\" does not exist")
@@ -134,16 +163,24 @@ exports.WordIndexFactory = function(){
     }
   };
 
-  /*
-    Function to search through created word indices
-  */
+  //Function to search through created word indices
   this.searchIndex = function(file, fullQuery, callback)
   {
+    //Check if argument is empty, if it so return last word index array.
+    if(file == undefined)
+    this.wordIndex = this.collection[this.collection.length - 1];
+
+    //Retrieve the array index with the file name
     this.wordIndex = this.collection[file];
+
+    //Variable to hold result
     let indices = [];
-    console.log(fullQuery);
+
+    //Check if the query is a string, if so split the words into an array.
     if(typeof(fullQuery) === typeof("tolu"))
     {var tokens  = fullQuery.split(" ");}
+
+    //Check if the query is an array, if so, flatten into a 1 level array.
     else if(Array.isArray(fullQuery))
     {
       var tokens = convertArray(fullQuery);
@@ -153,12 +190,18 @@ exports.WordIndexFactory = function(){
       console.log("Invalid input type. Please try again.");
       return undefined;
     }
+
+    //Loop through the array
     for(let i = 0; i < tokens.length; i++)
     {
       let found = false;
       let query = tokens[i].toString().replace(/[^a-zA-Z 0-9]+/g,'');
       console.log("Searching for \"" + query + "\"..... ");
 
+      /*
+      *Loop through the the word index array, compare words and add array of
+      *indices from word index to the result array if found.
+      */
       for(let j = 0; j< this.wordIndex.length;j++)
       {
         for(let item in this.wordIndex[j])
@@ -167,8 +210,6 @@ exports.WordIndexFactory = function(){
             {
               found = true;
               indices = this.wordIndex[j][item];
-              //console.log(indices);
-              //console.log(indices[1]);
               if(this.wordIndex[j][item].length === 1)
               {
                 console.log("Found \"" + query + "\" in JSON object " + this.wordIndex[j][item]
@@ -200,12 +241,12 @@ exports.WordIndexFactory = function(){
         if(found === false)
         console.log("\"" + query + "\" was not found. \n");
       }
+
+    //Return the array of inicies.
     callback(null, indices);
   };
 
-  /*
-    Function to flatten multiple arrays into one array
-  */
+  //Function to flatten multiple arrays into one array
   function convertArray(value)
   {
     if(Array.isArray(value))
